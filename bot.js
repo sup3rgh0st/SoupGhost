840d4951105f9bfebc7bbd6159d2ba5b3b0601ca
sup3rgh0st <sup3rgh0st@users.noreply.github.com>
2018-04-22 02:17:05 -0500

    Imgur I'm Feeling Lucky
    Imgur I'm Feeling Lucky

--- bot.js
+++ bot.js
@@ -1,6 +1,11 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+// npm install image-size --save
+var url = require('url');
+var http = require('http');
+var sizeOf = require('image-size');
+
 const heartbeat_duration = 1000; // milliseconds
 
 // SNACK BIN VARIABLES //
@@ -14,6 +19,24 @@ function snackbin_msg(message, channel, timestamp, reactstate) {
 }
 
 var clocks = ["🕛","🕒","🕕","🕘"];
+var imgurRemarks = [
+"Here's a fresh one!",
+"Here you go!",
+"Hope it's not a dick!",
+"Hope it's some titties!",
+"Not my finest hour...",
+"You can judge me all you want",
+"You might want to avert your eyes for this one...",
+"deja vu!",
+"deja vu!",
+"What would your mother think?",
+"What would your grandmother think?",
+"Hope there's nobody looking behind you!",
+"I think you mean ranDUMB, am I right?",
+"I swear it's actually random!!",
+"I think this breaks Imgur's TOS...",
+"This one is super special to me."
+];
 
 var snackbin = new Map();
 
@@ -24,11 +47,9 @@ client.on('ready', () => {
 
 client.on('message', msg => {
 	
-	if (msg.channel.name == "snackbin") { // 🕛
+	if (msg.channel.name == "snackbin") {
 		msg.react("🕛");
-		//var temp = ;
 		snackbin.set(new snackbin_msg(msg,msg.channel,0,0),0);
-		//msg.delete(10000);
 	}
 	if (msg.content.substring(0, 1) == '&') {
         var args = msg.content.substring(1).split(' ');
@@ -36,9 +57,20 @@ client.on('message', msg => {
        
         args = args.splice(1);
         switch(cmd) {
-            // !ping
             case 'ping':
                 msg.reply('Pong!');
+				break;
+			case 'help':
+				msg.channel.send(
+					"SoupGhost Help Menu\n" +
+					"made by sup3rgh0st\n" +
+					"=== COMMANDS ===\n" +
+					"imfeelinglucky: Find a Random Imgur image\n" +
+					"ping: pong!\n" +
+					"status: Server Status\n" +
+					"=== ADVICE ===\n" +
+					"Soup is like food you can drink."
+				);
 				break;
 			case 'status':
 				msg.channel.send(
@@ -48,7 +80,9 @@ client.on('message', msg => {
 					"sb_lifetime: " + sb_lifetime + "ms\n"
 				);
 				break;
-            // Just add any case commands if you want to..
+			case 'imfeelinglucky':
+				replyRandomImgurLink(msg, 1);
+				break;
          }
      }
 });
@@ -56,8 +90,6 @@ client.on('message', msg => {
 function heartbeat(){
 	//console.log(`Heartbeat`); // Debug
 	// Start of Function
-	
-	//console.log(client.channels.find("snackbin").type);
 	
 	snackbin.forEach(function(value, key){
 		key.timestamp = key.timestamp + heartbeat_duration;
@@ -81,11 +113,47 @@ function heartbeat(){
 				});
 				key.message.react(clocks[key.reactstate]);
 			}
-			
 		}
 	});
 	// End of Function
 	setTimeout(heartbeat, heartbeat_duration);
+}
+
+function replyRandomImgurLink(msg, iter) {
+	
+	// If we reach this, we're either unlucky, or something is broken. Either way, bail.
+	if(iter == 20){
+		msg.channel.send("Please try again!!");
+	}
+	var randURL = generateImgurURL();
+	
+	var options = url.parse(randURL);
+	http.get(options, function (response) {
+		var chunks = [];
+		response.on('data', function (chunk) {
+			chunks.push(chunk);
+		}).on('end', function() {
+			if(chunks.length !== 0){
+				// Found a valid image
+				var buffer = Buffer.concat(chunks);
+				console.log("Feeling Lucky : " + iter + " tries with URL " + randURL);
+				msg.channel.send("Random Image? " + imgurRemarks[Math.floor(Math.random() * imgurRemarks.length)], {files: [randURL]});
+			} else {
+				// Image wasn't valid, recurse
+				replyRandomImgurLink(msg, iter + 1)
+			}
+		});
+	});
+}
+
+// Generate an Imgur URL
+function generateImgurURL() {
+	var alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+	var url = "http://i.imgur.com/";
+	for (var i = 0; i < 5; i++) {
+		url = url.concat(alpha[Math.floor(Math.random() * alpha.length)]);
+	}
+	return url.concat('.jpg');
 }
 
 client.login('TOKEN HERE');
